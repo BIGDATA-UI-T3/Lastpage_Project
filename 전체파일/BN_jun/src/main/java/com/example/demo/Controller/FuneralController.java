62c3542d3ac86f5a8e3bd978fbcc742768041bb2
wnsgudqkr11 <wnsdgudqkr11@naver.com>
2025-11-09 16:10:34 +0900

    장소 정보, 목록 클릭 수정

--- 전체파일/BN_jun/src/main/java/com/example/demo/Controller/FuneralController.java
+++ 전체파일/BN_jun/src/main/java/com/example/demo/Controller/FuneralController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import java.util.List;
 
 @Controller
-public class FuneralController {
+public class    FuneralController {
 
     // application.properties의 ncp.map.client-id 값을 주입받음
     @Value("${ncp.map.client-id}")
@@ -34,39 +34,38 @@ public class FuneralController {
         if (funeralPlaceRepository.count() == 0) {
             List<FuneralPlace> initialPlaces = List.of(
                     new FuneralPlace(
-                            "라스트페이지 서울 본점",
-                            // [수정] 네이버에서 확실히 인식하는 주소(강남역)로 변경
-                            "서울특별시 강남구 강남대로 396",
-                            "https://www.lastpage-seoul.com", // 홈페이지 URL
-                            "02-1111-2222",
+                            "리틀포즈 반려동물 장례식장",
+                            "대구 군위군 부계면 부흥로 118",
+                            "https.littlepaws.co.kr",
+                            "054-382-0400",
                             List.of("기본 장례", "스톤 제작", "24시간")
                     ),
                     new FuneralPlace(
-                            "라스트페이지 경기 용인점",
-                            "경기도 용인시 수지구 포은대로 499",
-                            "https://www.lastpage-yongin.com",
-                            "031-3333-4444",
+                            "아이들랜드",
+                            "경북 경산시 와촌면 불굴사길 82",
+                            "httpshttps.아이들랜드.com/",
+                            "1522-6979",
                             List.of("기본 장례", "메모리얼 액자")
                     ),
                     new FuneralPlace(
-                            "라스트페이지 부산 해운대점",
-                            "부산광역시 해운대구 마린시티2로 38",
-                            "https://www.lastpage-busan.com",
-                            "051-5555-6666",
+                            "리멤버 반려동물장례식장",
+                            "경기 용인시 처인구 남사읍 원암로 535",
+                            "http://www.리멤버.net",
+                            "080-200-5004",
                             List.of("기본 장례", "해양 산골", "24시간")
                     ),
                     new FuneralPlace(
-                            "라스트페이지 대구 수성점",
-                            "대구광역시 수성구 동대구로 380",
-                            "https://www.lastpage-daegu.com",
-                            "053-7777-8888",
+                            "전주 반려동물 장례식장 아리움",
+                            "전북 전주시 완산구 콩쥐팥쥐로 1705-44",
+                            "http://www.arium.kr",
+                            "0507-1375-7945",
                             List.of("기본 장례", "스톤 제작")
                     ),
                     new FuneralPlace(
-                            "라스트페이지 광주 상무점",
-                            "광주광역시 서구 상무대로 773",
-                            "https://www.lastpage-gwangju.com",
-                            "062-9999-0000",
+                            "21그램 반려동물장례식장 경기광주1호점",
+                            "경기 광주시 매자리길 185-35",
+                            "https.21gram.co.kr/",
+                            "1688-1240",
                             List.of("기본 장례", "추모 영상 제작")
                     )
             );
