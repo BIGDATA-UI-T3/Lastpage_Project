f34d4d9b66b7d55d0d9b49cc23fa727dcf5ad234
B735244 <80499331+B735244@users.noreply.github.com>
2025-11-26 09:46:57 +0900

    Delete 전체파일 directory

--- 전체파일/Lastpage_Project/src/main/java/com/example/demo/Repository/OurpageReserveRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.Repository;
-
-import com.example.demo.Domain.Common.Entity.OurpageReserve;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OurpageReserveRepository extends JpaRepository<OurpageReserve, Long> {
-    // 필요한 경우 추가 메서드 작성
-}
